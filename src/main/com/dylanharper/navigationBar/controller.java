54017b366d1fc6149edb357bfe824f6fdd1d040a
SpaceCowboy147 <dylankharper147@gmail.com>
2022-12-28 14:29:07 -0700

    wth

--- src/main/com/dylanharper/navigationBar/controller.java
+++ src/main/com/dylanharper/navigationBar/controller.java
@@ -3,18 +3,12 @@ package com.dylanharper.navigationBar;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Controller
 public class controller {
