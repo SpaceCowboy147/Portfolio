f29e8decb3557467f00f9dd28ac3e6861fa60ea0
SpaceCowboy147 <dylankharper147@gmail.com>
2023-01-16 10:52:23 -0700

    adding stuff to html. also added meta tag so hopefully this will display correctly on mobile

--- src/main/java/com/dylanharper/navigationBar/controller.java
+++ src/main/java/com/dylanharper/navigationBar/controller.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.*;
+import java.nio.file.Files;
 
 @Controller
 public class controller {
@@ -41,15 +42,17 @@ public class controller {
     public void downloadResume(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
 
-         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("/DylanHarper_resume.pdf");
+         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("\\DylanHarper_resume.pdf");
             byte[] buffer = new byte[inputStream.available()];
             inputStream.read(buffer);
 
             File targetFile = new File("/DylanHarper_resume.pdf");
             OutputStream outStream = new FileOutputStream(targetFile);
             outStream.write(buffer);
+            if (targetFile.exists()) {
+                Files.copy(targetFile.toPath(), response.getOutputStream());
+            }
 
-            System.out.println("orange");
         } catch (Exception ex) {
 
             ex.printStackTrace();
