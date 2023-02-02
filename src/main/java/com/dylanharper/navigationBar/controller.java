a276e02fd2ba35c2d0f08bd92c74bebf21398a57
SpaceCowboy147 <dylankharper147@gmail.com>
2023-02-02 11:42:48 -0700

    chatGPT helped me get my jar and pdf downloads working correctly on a hosted app.

--- src/main/java/com/dylanharper/navigationBar/controller.java
+++ src/main/java/com/dylanharper/navigationBar/controller.java
@@ -1,13 +1,15 @@
 package com.dylanharper.navigationBar;
 
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.*;
-import java.nio.file.Files;
+import java.io.IOException;
 
 @Controller
 public class controller {
@@ -39,27 +41,29 @@ public class controller {
     }
 
     @GetMapping("/DylanHarper_resume.pdf")
-    public void downloadResume(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try {
+        public ResponseEntity<InputStreamResource> downloadPDF() {
+            try {
+                ClassPathResource pdfFile = new ClassPathResource("//templates/DylanHarper_resume.pdf");
 
-         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("\\DylanHarper_resume.pdf");
-            byte[] buffer = new byte[inputStream.available()];
-            inputStream.read(buffer);
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_PDF);
+                headers.add("Content-Disposition", "attachment; filename= DylanHarper_resume.pdf");
+                headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+                headers.add("Pragma", "no-cache");
+                headers.add("Expires", "0");
 
-            File targetFile = new File("/DylanHarper_resume.pdf");
-            OutputStream outStream = new FileOutputStream(targetFile);
-            outStream.write(buffer);
-            if (targetFile.exists()) {
-                Files.copy(targetFile.toPath(), response.getOutputStream());
-            }
+                return ResponseEntity
+                        .ok()
+                        .headers(headers)
+                        .contentLength(pdfFile.contentLength())
+                        .contentType(MediaType.APPLICATION_PDF)
+                        .body(new InputStreamResource(pdfFile.getInputStream()));
+            } catch (IOException ex) {
+                return ResponseEntity.badRequest().build();
 
-        } catch (Exception ex) {
-
-            ex.printStackTrace();
         }
-
-            }
-            }
+    }
+}
 
 
 
--- src/main/java/com/dylanharper/webApps/webAppController.java
+++ src/main/java/com/dylanharper/webApps/webAppController.java
@@ -2,21 +2,47 @@ package com.dylanharper.webApps;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.nio.file.Files;
 
 @Controller
 public class webAppController {
 
-    @GetMapping("/templates/Calculator.jar")
-    public void doGet() throws IOException {
-        File jarPath = new File("C:\\Users\\dylan\\Documents\\GitHub\\portfolio\\src\\main\\resources\\templates\\Calculator.jar");
-        Runtime.getRuntime().exec("java -jar ./Calculator.jar");
-    }
+
+        @GetMapping("/templates/Calculator.jar")
+        public ResponseEntity<InputStreamResource> downloadJar() {
+            try {
+                ClassPathResource jarFile = new ClassPathResource("jars/sample.jar");
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                headers.add("Content-Disposition", "attachment; filename=sample.jar");
+                headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+                headers.add("Pragma", "no-cache");
+                headers.add("Expires", "0");
+
+                return ResponseEntity
+                        .ok()
+                        .headers(headers)
+                        .contentLength(jarFile.contentLength())
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .body(new InputStreamResource(jarFile.getInputStream()));
+            } catch (IOException ex) {
+                return ResponseEntity.badRequest().build();
+            }
+        }
+
+
 
 
     @GetMapping("/login")
